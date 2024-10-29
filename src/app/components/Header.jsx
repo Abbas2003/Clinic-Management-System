3308ed97cf545f6be8375714627adfe66e8e6a30
Abbas2003 <abbas.mohammad805@gmail.com>
2024-10-30 01:01:18 +0500

    Header added

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarTrigger,
+} from "@/components/ui/menubar"
+
+
+
+const Header = () => {
+    return (
+        <section className='container mx-auto flex jusitify-between p-4'>
+            <div>Logo</div>
+            <div>
+                <Menubar>
+                    <MenubarMenu>
+                        <MenubarTrigger>File</MenubarTrigger>
+                        <MenubarContent>
+                            <MenubarItem>
+                                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                            </MenubarItem>
+                            <MenubarItem>New Window</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Share</MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>Print</MenubarItem>
+                        </MenubarContent>
+                    </MenubarMenu>
+                </Menubar>
+
+            </div>
+        </section>
+    )
+}
+
+export default Header
