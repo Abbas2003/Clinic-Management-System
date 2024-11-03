f01f4037749bb54d968435f60161f746b9d38bf6
Abbas2003 <abbas.mohammad805@gmail.com>
2024-11-03 23:21:14 +0500

    Next Auth implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,37 +10,61 @@ import {
 } from "@/components/ui/menubar"
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { ModeToggle } from '@/components/dark-theme-toggler'
+import { auth } from '../../auth'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import Image from 'next/image'
 
 
 
-const Header = () => {
+const Header = async () => {
+
+    const session = await auth()
+    console.log(session);
+
+
     return (
         <section className='container mx-auto flex items-center justify-between p-4 border-b'>
             <div className='text-2xl font-serif'>Logo</div>
-
             <div className='flex items-center gap-3'>
                 <div>
-                    <Menubar className="rounded-full border border-gray-300 p-1 flex items-center justify-center">
-                        <MenubarMenu>
-                            <MenubarTrigger className="rounded-full">
-                                <Avatar>
-                                    <AvatarImage src="https://github.com/shadcn.png" />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
+                    {session ? (
+                        <Menubar className="rounded-full border border-gray-300 p-1 flex items-center justify-center">
+                            <MenubarMenu>
+                                <MenubarTrigger className="border-none bg-transparent p-0 m-0">
+                                    <Image
+                                        src={session?.user?.image}
+                                        height={40}
+                                        width={40}
+                                        className="rounded-full"
+                                    />
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <Link href={'/profile'}>
+                                        <MenubarItem>
+                                            Profile <MenubarShortcut>⌘P</MenubarShortcut> 
+                                        </MenubarItem>
+                                    </Link>
 
-                            </MenubarTrigger>
-                            <MenubarContent>
-                                <MenubarItem>
-                                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarItem>New Window</MenubarItem>
-                                <MenubarSeparator />
-                                <MenubarItem>Share</MenubarItem>
-                                <MenubarSeparator />
-                                <MenubarItem>Print</MenubarItem>
-                            </MenubarContent>
-                        </MenubarMenu>
-                    </Menubar>
+                                    <Link href={'/appointments'}>
+                                        <MenubarItem>
+                                            My Appointments <MenubarShortcut>⌘A</MenubarShortcut> 
+                                        </MenubarItem>
+                                    </Link>
+                                    <MenubarSeparator />
+
+                                    <MenubarItem>
+                                        Logout <MenubarShortcut>⌘L</MenubarShortcut> 
+                                    </MenubarItem>
+                                </MenubarContent>
+
+                            </MenubarMenu>
+                        </Menubar>
+                    ) : (
+                        <Link href={"/signin"}>
+                            <Button variant={"outline"}>Login</Button>
+                        </Link>
+                    )}
                 </div>
                 <ModeToggle />
             </div>
